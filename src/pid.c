fd00c05884636cc1b2663e30de4bb2809b937833
qianqiangliu <qianqiangliu@hotmail.com>
2016-05-17 11:01:47 +0800

    fix closedir bug

--- src/pid.c
+++ src/pid.c
@@ -3,15 +3,20 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <dirent.h>
+#include <errno.h>
 
+#include "debug.h"
 #include "pid.h"
 
 #define READ_BUF_SIZE	512
 
 /*
  * @pname: process name
- * @pid  : array for pid
- * @size : the size of array
+ * @pid: array for pid
+ * @size: the size of array
+ * @return: -1: error occured,
+ *           0: can not found,
+ *          >0: found
  */
 int find_pid_by_name(const char *pname, pid_t *pid, int size)
 {
@@ -21,7 +26,7 @@ int find_pid_by_name(const char *pname, pid_t *pid, int size)
 
 	dir = opendir("/proc");
 	if (!dir) {
-		printf("Cannot open /proc");
+		debug(LOG_ERR, "opendir error: %s", strerror(errno));
 		return -1;
 	}
 
@@ -54,10 +59,13 @@ int find_pid_by_name(const char *pname, pid_t *pid, int size)
 		if (strcmp(pname, name) == 0) {
 			if (i <= size - 1)
 				pid[i++] = strtol(next->d_name, NULL, 0);
-			else
-				return size;
+			else {
+				i = size;
+				break;
+			}
 		}
 	}
+	closedir(dir);
 
 	return i;
 }
