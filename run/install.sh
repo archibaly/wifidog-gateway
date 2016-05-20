0119dfb7555e1018b9e3f7b8c17d33b253e2bb69
qianqiangliu <qianqiangliu@hotmail.com>
2016-05-20 16:45:20 +0800

    modify run package

--- run/install.sh
+++ run/install.sh
@@ -1,28 +1,11 @@
 #!/bin/sh
 
-LINES=29
+LINES=12
 TAR=/tmp/wifidog.tgz
-
-wifidog=`cat /etc/rc.local | grep wifidog`
-if [ -z "$wifidog" ]; then
-	sed -i '/exit 0/i\wifidog' /etc/rc.local
-fi
-
-prodaemon=`cat /etc/rc.local | grep prodaemon`
-if [ -z "$prodaemon" ]; then
-	sed -i '/exit 0/i\prodaemon' /etc/rc.local
-fi
 
 tail -n +$LINES $0 > $TAR
 
-tar -zxvf $TAR -C /bin > /dev/null
-mv /bin/wifidog.conf /etc
-mv /bin/wifidog-msg.html /etc
+tar -zxvf $TAR -C / > /dev/null
 rm -f $TAR
-
-mv /bin/libhttpd.so.0.0.0 /lib
-cd /lib
-ln -sf libhttpd.so.0.0.0 libhttpd.so.0
-ln -sf libhttpd.so.0 libhttpd.so
 
 exit 0
--- run/package.sh
+++ run/package.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-cp ../bin/wdctl .
-cp ../bin/wifidog .
-cp ../bin/prodaemon .
-cp ../lib/libhttpd.so.0.0.0 .
+cp ../bin/wdctl bin
+cp ../bin/wifidog bin
+cp ../wifidog.conf etc
+cp ../wifidog-msg.html etc
+cp ../lib/libhttpd.so.0.0.0 lib
 
-mips-openwrt-linux-strip wdctl
-mips-openwrt-linux-strip wifidog
-mips-openwrt-linux-strip prodaemon
-mips-openwrt-linux-strip libhttpd.so.0.0.0
+mips-openwrt-linux-strip bin/*
+mips-openwrt-linux-strip lib/libhttpd.so.0.0.0
 
-tar zcvf wifidog.tgz wdctl wifidog prodaemon wifidog.conf wifidog-msg.html libhttpd.so.0.0.0
+tar zcvf wifidog.tgz bin etc lib
 cat install.sh wifidog.tgz > wifidog.run
 
 chmod +x wifidog.run
